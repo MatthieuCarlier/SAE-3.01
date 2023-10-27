390f23a0532bc71790f0a9d60fc2ac15a00b0a02
MatthieuCarlier <144430700+MatthieuCarlier@users.noreply.github.com>
2023-10-27 09:12:14 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src-2/**/*.{html,js}"],
   theme: {
     extend: {
       colors: {
